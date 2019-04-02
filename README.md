40a3d9d89a84bec2577b50b090fefd84a5efb302
Jarred Parr <jparr721@gmail.com>
2019-04-02 09:34:07 -0400

    it works

---
+++ README.md
@@ -0,0 +1,19 @@
+# Deep frier
+The tool no one should ever use... ever.
+
+## Compiling
+### Requirements
+- Armadillo 9.3
+- OpenCV
+- cmake
+- Linux
+
+### Installing
+```
+$ mkdir build
+$ cd build
+$ cmake ..
+$ make
+$ cd bin
+$ ./fry <image>
+```
---
+++ cookout.cc
@@ -0,0 +1,82 @@
+#include <armadillo>
+#include <iostream>
+#include <tuple>
+#include <numeric>
+#include <stdexcept>
+#include <vector>
+#include <opencv2/opencv.hpp>
+
+#define yeet throw
+
+struct Image {
+  const std::string img_path;
+  const std::string output_path;
+  arma::mat image;
+
+  Image(const std::string& img_path, const std::string& output_path="") : img_path(img_path), output_path(output_path) {
+    cv::Mat temp_image = cv::imread(img_path);
+    cv::Mat temp_image_2;
+
+    // Convert to floating point image for SVD
+    temp_image.convertTo(temp_image_2, CV_64F, 1.0/255);
+
+    arma::mat image(reinterpret_cast<double*>(temp_image_2.data), temp_image_2.rows, temp_image_2.cols);
+    this->image = image;
+  }
+};
+
+arma::mat approximation(arma::mat image) {
+  // Anything below our threshold we do not care about
+  const int relevancy_threshold{1};
+
+  // Our U matrix
+  arma::mat U;
+  // Our sigma vector of singular values
+  arma::vec S;
+  // Our non-transposed V matrix
+  arma::mat V;
+
+  // Calculate our singular value decomposition
+  arma::svd(U, S, V, image);
+
+  int k = std::accumulate(S.begin(), S.end(), 0,
+      [](int count, const int& v) { return v > relevancy_threshold ? ++count : count; });
+
+  arma::mat Sigma(arma::size(image), arma::fill::zeros);
+
+  for (int i = 0; i < k; ++i) {
+    Sigma.at(i, i) = S(i);
+  }
+
+  auto sv = Sigma * V;
+  return U * sv;
+}
+
+void draw_that_boy(arma::mat image) {
+  const auto rows = image.n_rows;
+  const auto cols = image.n_cols;
+
+
+  // Convert now to opencv to display
+  cv::Mat cv_img(rows, cols, CV_64FC1, image.memptr());
+  cv::imshow("FRESH OUTTA THE FRIER", cv_img);
+  cv::waitKey();
+}
+
+void help() {
+  std::cout << "dEeP.fRIeD\t\t Usage" << std::endl;
+  std::cout << "cookout <img_path>\t\t\t\t - Compress the shit out of an image in place" << std::endl;
+  std::cout << "coolout <img_path> <output_path>\t\t - Compress the shit out of an image and save to another one" << std::endl;
+}
+
+int main(int argc, char** argv) {
+  if (argc < 2) {
+    help();
+  }
+
+  switch(argc) {
+    case 2:
+      Image img(argv[1]);
+      draw_that_boy(approximation(img.image));
+  }
+}
